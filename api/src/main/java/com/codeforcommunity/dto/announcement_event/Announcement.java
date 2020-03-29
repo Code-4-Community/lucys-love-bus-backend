7e2ae2f1066dd2966ec0b6938b9dcd746754fc14
Aaron Pradhan <apradhan@tripadvisor.com>
2020-03-29 13:41:13 -0400

    Add constructor, getters for Announcements to allow serialization

--- api/src/main/java/com/codeforcommunity/dto/announcement_event/Announcement.java
+++ api/src/main/java/com/codeforcommunity/dto/announcement_event/Announcement.java
@@ -9,4 +9,28 @@ public class Announcement {
   private String description;
   private Timestamp created;
 
+  private Announcement() {}
+
+  public Announcement(int id, String title, String description, Timestamp created) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.created = created;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Timestamp getCreated() {
+    return created;
+  }
 }
