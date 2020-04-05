5eca1be359d506171e12239b0114e3b319e4fb30
Aaron Pradhan <apradhan@tripadvisor.com>
2020-04-05 11:55:24 -0400

    Made ID serial in SQL

--- persist/src/main/resources/db/migration/V2__AnnouncementsTables.sql
+++ persist/src/main/resources/db/migration/V2__AnnouncementsTables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE announcements (
-    id int NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS announcements (
+    id serial PRIMARY KEY,
     title varchar NOT NULL,
     description varchar NOT NULL,
     created timestamp DEFAULT CURRENT_TIMESTAMP
