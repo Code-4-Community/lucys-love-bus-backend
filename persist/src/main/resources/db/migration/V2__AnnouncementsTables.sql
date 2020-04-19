d226ae4890db21a52e38d13e90a315a5e2de7fb5
Aaron Pradhan <apradhan@tripadvisor.com>
2020-04-19 11:37:00 -0400

    Add event_id column

--- persist/src/main/resources/db/migration/V2__AnnouncementsTables.sql
+++ persist/src/main/resources/db/migration/V2__AnnouncementsTables.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS announcements (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     description varchar NOT NULL,
-    created timestamp DEFAULT CURRENT_TIMESTAMP
+    created timestamp DEFAULT CURRENT_TIMESTAMP,
+    event_id int DEFAULT NULL
 );
