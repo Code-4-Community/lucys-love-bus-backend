1ecae7da21987698ff50e167e4a80f54571b68d2
Jack Blanc <jblanc222@gmail.com>
2020-04-24 21:14:48 -0400

    /api/v1/protected/checkout now adds a Checkout Session needs to return the sessionId endpoint needs to accept actual line items

--- api/src/main/java/com/codeforcommunity/rest/subrouter/CheckoutRouter.java
+++ api/src/main/java/com/codeforcommunity/rest/subrouter/CheckoutRouter.java
@@ -11,6 +11,10 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import java.util.*;
+
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
 
 import static com.codeforcommunity.rest.ApiRouter.end;
 
@@ -32,11 +36,48 @@ public class CheckoutRouter implements IRouter {
     }
 
     private void registerCheckoutSession(Router router) {
-        Route getRequestsRoute = router.get("/checkout");
+        Route getRequestsRoute = router.post("/");
         getRequestsRoute.handler(this::handleCheckoutSession);
     }
 
     private void handleCheckoutSession(RoutingContext ctx) {
+        Stripe.apiKey = "sk_test_Q2wTkIY5Z3h9pjtgkksJULj200M84LsI3q";
+
+        List<Object> paymentMethodTypes =
+                new ArrayList<>();
+        paymentMethodTypes.add("card");
+        List<Object> lineItems = new ArrayList<>();
+        Map<String, Object> lineItem1 = new HashMap<>();
+        lineItem1.put("name", "T-shirt");
+        lineItem1.put(
+                "description",
+                "Comfortable cotton t-shirt"
+        );
+        lineItem1.put("amount", 1500);
+        lineItem1.put("currency", "usd");
+        lineItem1.put("quantity", 2);
+        lineItems.add(lineItem1);
+        Map<String, Object> params = new HashMap<>();
+        params.put(
+                "success_url",
+                "https://example.com/success"
+        );
+        params.put(
+                "cancel_url",
+                "https://example.com/cancel"
+        );
+        params.put(
+                "payment_method_types",
+                paymentMethodTypes
+        );
+        params.put("line_items", lineItems);
+
+        try {
+            Session session = Session.create(params);
+        } catch (Exception e) {
+            end(ctx.response(), 500, "Failed to create session");
+        }
+
         end(ctx.response(), 200, "JsonObject.mapFrom(response).encode()");
     }
 
