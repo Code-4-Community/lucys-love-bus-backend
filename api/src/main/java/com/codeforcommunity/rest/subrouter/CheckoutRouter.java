4a6bf275d462106dd4a208a0f59d4881bce27585
Jack Blanc <jblanc222@gmail.com>
2020-04-26 12:03:11 -0400

    removing events

--- api/src/main/java/com/codeforcommunity/rest/subrouter/CheckoutRouter.java
+++ api/src/main/java/com/codeforcommunity/rest/subrouter/CheckoutRouter.java
@@ -1,14 +1,9 @@
 package com.codeforcommunity.rest.subrouter;
 
-import com.codeforcommunity.api.IEventsProcessor;
-import com.codeforcommunity.auth.JWTData;
 import com.codeforcommunity.dto.checkout.PostCheckoutRequest;
-import com.codeforcommunity.dto.events.CreateEventRequest;
-import com.codeforcommunity.dto.events.SingleEventResponse;
 import com.codeforcommunity.rest.IRouter;
 import com.codeforcommunity.rest.RestFunctions;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -21,11 +16,8 @@ import static com.codeforcommunity.rest.ApiRouter.end;
 
 public class CheckoutRouter implements IRouter {
 
-    private final IEventsProcessor processor;
 
-    public CheckoutRouter(IEventsProcessor processor) {
-        this.processor = processor;
-    }
+    public CheckoutRouter() {}
 
     @Override
     public Router initializeRouter(Vertx vertx) {
