386e3b451a91c61e2548d7fc11a3af2fe62a1fe3
LukeBoyer <boyer.l@husky.neu.edu>
2020-05-03 12:38:42 -0400

    remove seeding script files for different pr

--- scripts/seeding/sql/admin_user.sql
+++
@@ -1,7 +0,0 @@
-begin;
-
-update users set privilege_level = 2 where first_name = 'admin';
-
-update users set verified = 1;
-
-commit;
--- scripts/seeding/sql/user_events_assoc.sql
+++
@@ -1,5 +0,0 @@
-begin;
-
-insert into user_events (select users.id as user_id, events.id as event_id from users, events);
-	
-commit;
