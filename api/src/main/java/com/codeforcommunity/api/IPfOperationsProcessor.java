3fafabfc8e376173b09aefc9365c6e4783081c37
LukeBoyer <boyer.l@husky.neu.edu>
2020-05-03 12:43:58 -0400

    commenting

--- api/src/main/java/com/codeforcommunity/api/IPfOperationsProcessor.java
+++ api/src/main/java/com/codeforcommunity/api/IPfOperationsProcessor.java
@@ -5,10 +5,6 @@ import com.codeforcommunity.dto.auth.SessionResponse;
 
 public interface IPfOperationsProcessor {
 
-	//TODO java doc
-
-	// signs a user up and makes a request for them to be PF. Will allow user to 
-	// go about there business as a GP until it is accepted ... 
 	SessionResponse signUpPF(NewUserAsPFRequest newUserAsPFRequest);
 
 }
