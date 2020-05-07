f4a7eaf9d39f8f38b9f5b84e6c3d865e7c28f542
Liam Moynihan <liammoyn@gmail.com>
2020-05-07 16:16:15 -0400

    Added java doc comment to protected user processor

--- api/src/main/java/com/codeforcommunity/api/IProtectedUserProcessor.java
+++ api/src/main/java/com/codeforcommunity/api/IProtectedUserProcessor.java
@@ -4,5 +4,8 @@ import com.codeforcommunity.auth.JWTData;
 
 public interface IProtectedUserProcessor {
 
+  /**
+   * Deletes the given user from the database. Does NOT invalidate the user's JWT tokens.
+   */
   void deleteUser(JWTData userData);
 }
