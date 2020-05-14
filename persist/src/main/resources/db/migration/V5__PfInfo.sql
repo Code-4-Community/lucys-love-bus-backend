c6492d627c689ac0b6fe0454a7cb251a1cd2c723
Liam Moynihan <liammoyn@gmail.com>
2020-05-13 20:34:29 -0400

    Updated auth table schema

--- persist/src/main/resources/db/migration/V5__PfInfo.sql
+++ persist/src/main/resources/db/migration/V5__PfInfo.sql
@@ -6,33 +6,46 @@ ALTER TABLE users
 	ADD COLUMN state VARCHAR(32);
 ALTER TABLE users
 	ADD COLUMN zipCode VARCHAR(16);
-ALTER TABLE users
-    ADD COLUMN phoneNumber varchar(16) NOT NULL;
-ALTER TABLE users
-	ADD COLUMN allergies TEXT;
 
-CREATE TABLE IF NOT EXISTS additional_contacts (
+ALTER TABLE users
+    DROP COLUMN first_name;
+ALTER TABLE users
+    DROP COLUMN last_name;
+
+CREATE TABLE IF NOT EXISTS contacts (
 	id SERIAL PRIMARY KEY,
 	user_id INT NOT NULL,
+	is_main_contact BOOLEAN NOT NULL DEFAULT false,
 
-	phoneNumber VARCHAR(16) NOT NULL,
-	email VARCHAR(36) NOT NULL,
-	shouldSendEmails BOOLEAN NOT NULL,
+    email VARCHAR(36) NOT NULL,
+    should_send_emails BOOLEAN NOT NULL DEFAULT false,
 
+    first_name VARCHAR(36) NOT NULL,
+    last_name VARCHAR(36) NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
+
+	phone_number VARCHAR(16),
+    pronouns VARCHAR(36),
 	allergies TEXT,
+	diagnosis TEXT,
+	medications TEXT,
+	notes TEXT,
 
-	CONSTRAINT additional_contacts_users_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+	CONSTRAINT contacts_users_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS children (
 	id SERIAL PRIMARY KEY,
 	user_id INT NOT NULL,
 
-	date_of_birth TIMESTAMP NOT NULL,
+    first_name VARCHAR(36) NOT NULL,
+    last_name VARCHAR(36) NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
 	pronouns VARCHAR(32) NOT NULL,
 
 	school_year VARCHAR(32),
 	school VARCHAR(64),
+
 	allergies TEXT,
 	diagnosis TEXT,
 	medications TEXT,
