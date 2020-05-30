81cdc36d1bfb515197a6a075276890b2643027ed
Aaron Pradhan <apradhan@tripadvisor.com>
2020-05-30 19:06:08 -0400

    Rename constraints

--- persist/src/main/resources/db/migration/V8__PendingRegistration.sql
+++ persist/src/main/resources/db/migration/V8__PendingRegistration.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS pending_registrations
     ticket_quantity_delta      integer NOT NULL CHECK (ticket_quantity_delta > 0),
     stripe_checkout_session_id varchar(64),
 
-    CONSTRAINT registration_user_fkey FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT registration_event_fkey FOREIGN KEY (event_id) REFERENCES events (id)
+    CONSTRAINT pending_user_fkey FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT pending_event_fkey FOREIGN KEY (event_id) REFERENCES events (id)
 );
