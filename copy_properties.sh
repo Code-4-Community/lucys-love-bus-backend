9785cabe13e8e424b938ef3543c7251ef0f0613a
Justin Konecny <konecnyjustin@gmail.com>
2020-09-02 19:05:54 -0400

    This will fix it im sure

---
+++ copy_properties.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Directory containing all example properties
+DIR_PROP='common/src/main/resources/properties/'
+
+cd $DIR_PROP || exit 1
+
+# Copy each .example file to a .properties file
+for EX_FILE in *.properties.example; do
+  PROP_FILE="$(echo "$EX_FILE" | cut -d "." -f 1).properties"
+  cp "$EX_FILE" "$PROP_FILE"
+done
