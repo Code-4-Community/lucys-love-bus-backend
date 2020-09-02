8585a36958426d940b70e1d082c4f490a9ff60b5
Justin Konecny <konecnyjustin@gmail.com>
2020-09-02 19:07:57 -0400

    Add comment

--- copy_properties.sh
+++ copy_properties.sh
@@ -8,5 +8,6 @@ cd $DIR_PROP || exit 1
 # Copy each .example file to a .properties file
 for EX_FILE in *.properties.example; do
   PROP_FILE="$(echo "$EX_FILE" | cut -d "." -f 1).properties"
+  echo "Copying $EX_FILE to $PROP_FILE"
   cp "$EX_FILE" "$PROP_FILE"
 done
