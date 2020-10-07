f3fb329e0f3a1686990b9dd85acc15e8a80916c9
Vedant Rautela <v.rautela01@gmail.com>
2020-10-06 21:33:14 -0400

    added Javadoc to the classes in the protected_user directory

--- api/src/main/java/com/codeforcommunity/dto/protected_user/ChangeEmailRequest.java
+++ api/src/main/java/com/codeforcommunity/dto/protected_user/ChangeEmailRequest.java
@@ -4,34 +4,68 @@ import com.codeforcommunity.api.ApiDto;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A request by a user to change the email address to which emails are sent.
+ */
 public class ChangeEmailRequest extends ApiDto {
 
   private String newEmail;
   private String password;
 
+  /**
+   * Creates a new ChangeEmailRequest where the user is requesting their email to be changed to
+   * {@code newEmail} and the password they have entered is {@code password}.
+   * @param newEmail the new email address
+   * @param password the password entered by the user
+   */
   public ChangeEmailRequest(String newEmail, String password) {
     this.newEmail = newEmail;
     this.password = password;
   }
 
+  /**
+   * Creates a ChangeEmailRequest with no information.
+   */
   private ChangeEmailRequest() {}
 
+  /**
+   * Returns the new email address.
+   * @return the new email address
+   */
   public String getNewEmail() {
     return newEmail;
   }
 
+  /**
+   * Updates the new email address.
+   * @param newEmail the new email address
+   */
   public void setNewEmail(String newEmail) {
     this.newEmail = newEmail;
   }
 
+  /**
+   * Returns the password entered by the user.
+   * @return the password entered by the user
+   */
   public String getPassword() {
     return password;
   }
 
+  /**
+   * Sets the password entered by the user
+   * @param password the new password entered by the user
+   */
   public void setPassword(String password) {
     this.password = password;
   }
 
+  /**
+   * Verify this ChangeEmailRequest is valid by checking the validity of the email and the password.
+   * @param fieldPrefix A string to prefix each field with (for use if this is a sub-field). Should
+   *     be of the form "OBJECT.".
+   * @return a list of strings with each string indicating one field that is invalid
+   */
   @Override
   public List<String> validateFields(String fieldPrefix) {
     String fieldName = fieldPrefix + "change_email_request.";
--- api/src/main/java/com/codeforcommunity/dto/protected_user/SetContactsAndChildrenRequest.java
+++ api/src/main/java/com/codeforcommunity/dto/protected_user/SetContactsAndChildrenRequest.java
@@ -6,14 +6,26 @@ import com.codeforcommunity.dto.protected_user.components.Contact;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A request to set the main and additional contacts of a group of children.
+ */
 public class SetContactsAndChildrenRequest extends ApiDto {
 
   private Contact mainContact;
   private List<Contact> additionalContacts;
   private List<Child> children;
 
+  /**
+   * Creates a SetContactsAndChildrenRequest with no information (all fields are null)
+   */
   public SetContactsAndChildrenRequest() {}
 
+  /**
+   * Creates a new SetContactsAndChildrenRequest with the given information
+   * @param mainContact the main contact for the children
+   * @param additionalContacts the list of additional contacts for the children
+   * @param children the list of children
+   */
   public SetContactsAndChildrenRequest(
       Contact mainContact, List<Contact> additionalContacts, List<Child> children) {
     this.mainContact = mainContact;
@@ -21,51 +33,82 @@ public class SetContactsAndChildrenRequest extends ApiDto {
     this.children = children;
   }
 
+  /**
+   * Returns the main contact
+   * @return the main contact
+   */
   public Contact getMainContact() {
     return mainContact;
   }
 
+  /**
+   * Updates the main contact
+   * @param mainContact the new main contact
+   */
   public void setMainContact(Contact mainContact) {
     this.mainContact = mainContact;
   }
 
+  /**
+   * Returns the additional contacts
+   * @return the additional contacts
+   */
   public List<Contact> getAdditionalContacts() {
     return additionalContacts;
   }
 
+  /**
+   * Updates the additional contacts
+   * @param additionalContacts the new additional contacts
+   */
   public void setAdditionalContacts(List<Contact> additionalContacts) {
     this.additionalContacts = additionalContacts;
   }
 
+  /**
+   * Returns the list of children
+   * @return the list of children
+   */
   public List<Child> getChildren() {
     return children;
   }
 
+  /**
+   * Updates the list of children
+   * @param children the new list of children
+   */
   public void setChildren(List<Child> children) {
     this.children = children;
   }
 
+  /**
+   * Verify this SetContactsAndChildrenRequest is valid by checking the validity of the main
+   * contact, additional contacts and children.
+   * @param fieldPrefix A string to prefix each field with (for use if this is a sub-field). Should
+   *     be of the form "OBJECT.".
+   * @return a list of strings with each string indicating one field that is invalid
+   */
   @Override
-  public List<String> validateFields(String fieldPrefx) {
-    String fieldName = fieldPrefx + "set_contacts_and_children_request.";
+  public List<String> validateFields(String fieldPrefix) {
+    String fieldName = fieldPrefix + "set_contacts_and_children_request.";
     List<String> fields = new ArrayList<>();
     if (mainContact == null) {
-      fields.add(fieldPrefx + "main_contact");
+      fields.add(fieldPrefix + "main_contact");
     } else {
-      fields.addAll(mainContact.validateFields(fieldPrefx));
+      fields.addAll(mainContact.validateFields(fieldPrefix));
     }
     if (children == null) {
-      fields.add(fieldPrefx + "children");
+      fields.add(fieldPrefix + "children");
     } else {
       for (Child child : children) {
-        fields.addAll(child.validateFields(fieldPrefx));
+        fields.addAll(child.validateFields(fieldPrefix));
       }
     }
     if (additionalContacts == null) {
-      fields.add(fieldPrefx + "additional_contacts");
+      fields.add(fieldPrefix + "additional_contacts");
     } else {
       for (Contact ac : additionalContacts) {
-        fields.addAll(ac.validateFields(fieldPrefx));
+        fields.addAll(ac.validateFields(fieldPrefix));
       }
     }
     return fields;
