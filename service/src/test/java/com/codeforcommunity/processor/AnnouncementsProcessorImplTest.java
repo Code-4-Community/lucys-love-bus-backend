25b4595ae3631b96b0db55a96b502076d91d33f9
Anirudh Singh <singh.anir@northeastern.edu>
2021-02-23 21:56:44 -0500

    tests for getting announcements with images + PR feedback
    added tests to get announcements with images

--- service/src/test/java/com/codeforcommunity/processor/AnnouncementsProcessorImplTest.java
+++ service/src/test/java/com/codeforcommunity/processor/AnnouncementsProcessorImplTest.java
@@ -1,6 +1,7 @@
 package com.codeforcommunity.processor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import com.codeforcommunity.JooqMock;
@@ -116,6 +117,72 @@ public class AnnouncementsProcessorImplTest {
 
     assertEquals(res.getTotalCount(), 1);
     assertEquals(res.getAnnouncements().get(0).getId(), 0);
+  }
+
+  // test getting announcements (with images) with range covering some events
+  @Test
+  public void testGetAnnouncementsWithImageSrc1() {
+    // craft the get request
+    GetAnnouncementsRequest req =
+        new GetAnnouncementsRequest(
+            new Timestamp(START_TIMESTAMP_TEST - 1000),
+            new Timestamp(END_TIMESTAMP_TEST + 1000),
+            2);
+
+    // mock the announcements inside the DB
+    AnnouncementsRecord announcement1 = myJooqMock.getContext().newRecord(Tables.ANNOUNCEMENTS);
+    announcement1.setId(0);
+    announcement1.setTitle("the first announcement title");
+    announcement1.setCreated(new Timestamp(START_TIMESTAMP_TEST));
+    announcement1.setDescription("the first announcement description");
+    announcement1.setImageSrc("https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg");
+    myJooqMock.addReturn("SELECT", announcement1);
+
+    GetAnnouncementsResponse res = myAnnouncementsProcessorImpl.getAnnouncements(req);
+
+    assertEquals(res.getTotalCount(), 1);
+    assertEquals(res.getAnnouncements().get(0).getId(), 0);
+    assertEquals(res.getAnnouncements().get(0).getImageSrc(),
+        "https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg");
+  }
+
+  // test getting announcements (with images) with range covering all events
+  @Test
+  public void testGetAnnouncementsWithImageSrc2() {
+    // craft the get request
+    GetAnnouncementsRequest req =
+        new GetAnnouncementsRequest(
+            new Timestamp(START_TIMESTAMP_TEST2 - 1000),
+            new Timestamp(END_TIMESTAMP_TEST + 1000),
+            2);
+
+    // mock the announcements inside the DB
+    AnnouncementsRecord announcement1 = myJooqMock.getContext().newRecord(Tables.ANNOUNCEMENTS);
+    announcement1.setId(0);
+    announcement1.setTitle("the first announcement title");
+    announcement1.setCreated(new Timestamp(START_TIMESTAMP_TEST));
+    announcement1.setDescription("the first announcement description");
+    announcement1.setImageSrc("https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg");
+
+    AnnouncementsRecord announcement2 = myJooqMock.getContext().newRecord(Tables.ANNOUNCEMENTS);
+    announcement2.setId(1);
+    announcement2.setTitle("the second announcement title");
+    announcement2.setCreated(new Timestamp(START_TIMESTAMP_TEST2));
+    announcement2.setDescription("the second announcement description");
+
+    List<AnnouncementsRecord> announcements = new ArrayList<>();
+    announcements.add(announcement1);
+    announcements.add(announcement2);
+    myJooqMock.addReturn("SELECT", announcements);
+
+    GetAnnouncementsResponse res = myAnnouncementsProcessorImpl.getAnnouncements(req);
+
+    assertEquals(res.getTotalCount(), 2);
+    assertEquals(res.getAnnouncements().get(0).getId(), 0);
+    assertEquals(res.getAnnouncements().get(1).getId(), 1);
+    assertEquals(res.getAnnouncements().get(0).getImageSrc(),
+        "https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg");
+    assertNull(res.getAnnouncements().get(1).getImageSrc());
   }
 
   // test posting an announcement without admin privileges
