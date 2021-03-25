e531db19b54f7931d0e1ed064ecd8c3fefaeb942
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-03-25 09:56:16 -0400

    modified the event registered users route and registration response object

--- api/src/main/java/com/codeforcommunity/dto/userEvents/components/Registration.java
+++ api/src/main/java/com/codeforcommunity/dto/userEvents/components/Registration.java
@@ -6,12 +6,22 @@ public class Registration {
   private final String lastName;
   private final String email;
   private final int ticketCount;
+  private final int userId;
+  private final int privilegeLevel;
+  private final String phoneNumber;
+  private final String profilePicture;
+  private final boolean photoRelease;
 
-  public Registration(String firstName, String lastName, String email, int ticketCount) {
+  public Registration(String firstName, String lastName, String email, int ticketCount, int userId, int privilegeLevel, String phoneNumber, String profilePicture, boolean photoRelease) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.ticketCount = ticketCount;
+    this.userId = userId;
+    this.privilegeLevel = privilegeLevel;
+    this.phoneNumber = phoneNumber;
+    this.profilePicture = profilePicture;
+    this.photoRelease = photoRelease;
   }
 
   /**
@@ -48,5 +58,45 @@ public class Registration {
    */
   public String getLastName() {
     return lastName;
+  }
+
+  /**
+   * Gets the ID of the registrant
+   * @return the user ID
+   */
+  public int getUserId() {
+    return userId;
+  }
+
+  /**
+   * Gets the privilege level for the registrant
+   * @return the privilege level
+   */
+  public int getPrivilegeLevel() {
+    return privilegeLevel;
+  }
+
+  /**
+   * Gets the phone number of the registrant
+   * @return the phone number
+   */
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  /**
+   * Gets the profile picture of the registrant
+   * @return the profile picture
+   */
+  public String getProfilePicture() {
+    return profilePicture;
+  }
+
+  /**
+   * Gets whether the registrant has consented to photo and video release
+   * @return whether the registrant has consented to photo and video release
+   */
+  public boolean getPhotoRelease() {
+    return photoRelease;
   }
 }
